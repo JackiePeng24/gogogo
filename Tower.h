16613084134978e7b4958ba682fd73681500e2b0
jackiepeng <3117163253@qq.com>
2025-06-27 14:03:30 +0800

    revise the Tower.h

--- Tower.h
+++ Tower.h
@@ -1,49 +1,79 @@
 #pragma once
 #include"BoardPosition.h"
+#include"GameConstants.h"
 class Tower {
+private:
+    int hp;
+    int attackRange;
+    int damage;
+    BoardPosition position;
+    bool whose;
+
 public:
-	virtual ~Tower() {};
-    
     //血量
-	virtual int getHP() const = 0;
-    virtual bool isAlive() const = 0;
-    virtual void takeDamage(int amount) = 0;// 受到伤害
+    int getHP() {return hp; }
+    bool isAlive() {return hp > 0; }
+    void takeDamage(int amount) {hp = (hp > amount) ? (hp - amount) : 0; };// 受到伤害
 
     // 位置信息
-    virtual BoardPosition getPosition() const = 0;
-    virtual void setPosition(const BoardPosition& pos) = 0;
+    BoardPosition getPosition() {return position; }
+    virtual void setPosition() const = 0;
 
-    // 塔类型标识
+    // 塔标识
     virtual bool isKingTower() const = 0;
     virtual bool isPrincessTower() const = 0;
+    bool isWhoseTower() const { return whose; }
+
 
     // 攻击能力
     virtual int getAttackRange() const = 0;
-    virtual int getDamage() const = 0;
+    int getDamage() { return damage; }
+
+    
 };
 
 class KingTower : public Tower {
+private:
     int hp;
+    int attackRange;
+    int damage;
     BoardPosition position;
+    bool whose;
 
 public:
-    KingTower(const BoardPosition& pos, const int initialHP)
-        : position(pos), hp(initialHP) {
+    KingTower(const BoardPosition& pos, const int initialHP, const int AttackRange, const int Damage, const bool Whose)
+        : position(pos), hp(initialHP), attackRange(AttackRange), damage(Damage), whose(Whose) {
     }
 
-    int getHP() const override { return hp; }
-    bool isAlive() const override { return hp > 0; }
-    void takeDamage(int amount) override {
-        hp = (hp > amount) ? (hp - amount) : 0;
+    void setPosition() const override {
+        if (whose) {
+            position = KING_POS_1;
+        }
     }
-
-    BoardPosition getPosition() const override { return position; }
-    void setPosition(const BoardPosition& pos) override { position = pos; }//这里应该是具体的位置
 
     bool isKingTower() const override { return true; }
     bool isPrincessTower() const override { return false; }
 
     // 攻击属性
-    int getAttackRange() const override { return 7; }
-    int getDamage() const override { return 100; }
+    int getAttackRange() const { return 4; }
+    int getDamage() const { return 100; }
+};
+
+class PrincessTower : public Tower {
+private:
+    int hp;
+    int attackRange;
+    int damage;
+    BoardPosition position;
+    bool whose;
+public:
+    PrincessTower(const BoardPosition& pos, const int initialHP, const int AttackRange, const int Damage)
+        :position(pos), hp(initialHP), attackRange(AttackRange), damage(Damage) {}
+
+    bool isKingTower() const override { return false; }
+    bool isPrincessTower() const override { return true; }
+
+    // 攻击属性
+    int getAttackRange() const override { return 2; }
+    int getDamage() const override { return 50; }
 };
